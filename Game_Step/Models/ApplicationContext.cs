1eae6af6161daff6df7cce1ec7e332153d5810e9
Betsq9 <betsq9@gmail.com>
2021-03-02 20:19:20 +0200

    -Add in applications context instance of a DbSet<Games> -Changed DefaultConnections to DefaultConnection -Changed Connection_Trusted to Trusted_Connection -Add Entity "Game"

--- Game_Step/Models/ApplicationContext.cs
+++ Game_Step/Models/ApplicationContext.cs
@@ -4,6 +4,8 @@ namespace Game_Step.Models
 {
     public class ApplicationContext : DbContext 
     {
+        public DbSet<Game> Games { get; set; }
+
         public ApplicationContext(DbContextOptions<ApplicationContext> contextOptions)
             :base(contextOptions)
         {
---
+++ Game_Step/Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game_Step.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Article { get; set; }
+    }
+}
