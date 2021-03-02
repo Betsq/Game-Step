7a0dd31440368a704e90a06fc372fafa4fda0c23
Betsq9 <betsq9@gmail.com>
2021-03-02 19:53:45 +0200

    Add NuGet package EntityFrameworkCore.Sql NuGet package EntityFrameworkCore.Tools Class ApplicationContext Connection String

---
+++ Game_Step/Models/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Game_Step.Models
+{
+    public class ApplicationContext : DbContext 
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> contextOptions)
+            :base(contextOptions)
+        {
+
+        }
+    }
+}
