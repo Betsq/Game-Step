a1ee7969aa9a4f99ce4ec190811ad5afa8a2dc48
Betsq9 <betsq9@gmail.com>
2021-03-02 21:54:16 +0200

    -Add GameController -Add create operation
    -Add folder "Game"
    -Add "Create" View
    -Add empty "Index" View

---
+++ Game_Step/Controllers/GameController.cs
@@ -0,0 +1,37 @@
+﻿using Game_Step.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Game_Step.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly ApplicationContext db;
+
+        public GameController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Game game)
+        {
+                await db.Games.AddAsync(game);
+                await db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+        }
+    }
+}
--- Game_Step/Models/Game.cs
+++ Game_Step/Models/Game.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Game_Step.Models
+﻿namespace Game_Step.Models
 {
     public class Game
     {
