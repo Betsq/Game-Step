76a8356f5c3affef6897f78dea5d0757a45030d8
Betsq9 <betsq9@gmail.com>
2021-03-02 18:21:00 +0200

    Добавьте файлы проекта.

---
+++ Game_Step/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Game_Step.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
