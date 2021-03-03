2ad9fb7ccb7503b6622449d8bc6c2a52e3d89616
Betsq9 <betsq9@gmail.com>
2021-03-03 21:14:43 +0200

    -Add "create" operations of developer -Some change Index -Add link to "Index" View of DeveloperController

--- Game_Step/Controllers/DeveloperController.cs
+++ Game_Step/Controllers/DeveloperController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Game_Step.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,31 @@ namespace Game_Step.Controllers
 {
     public class DeveloperController : Controller
     {
+        private readonly ApplicationContext db;
+
+        public DeveloperController(ApplicationContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Developer developer)
+        {
+            await db.Developers.AddAsync(developer);
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
