42715a9207d5cb5f3b651d3493edd57e4995ef17
Betsq9 <betsq9@gmail.com>
2021-03-03 13:48:09 +0200

    -Add some property

---
+++ Game_Step/Migrations/20210303114551_AddSomeNewProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Game_Step.Migrations
+{
+    public partial class AddSomeNewProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Games");
+        }
+    }
+}
--- Game_Step/Models/Game.cs
+++ Game_Step/Models/Game.cs
@@ -1,8 +1,16 @@
-﻿namespace Game_Step.Models
+﻿using System;
+
+namespace Game_Step.Models
 {
     public class Game
     {
         public int Id { get; set; }
         public string Article { get; set; }
+        public string Language { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Publisher { get; set; }
+        public string Developer { get; set; }
+        public string Features { get; set; }
+        public string Region { get; set; }
     }
 }
