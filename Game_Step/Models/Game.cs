165e81ab931eae71e119f3381732963ca0fb98a3
Betsq9 <betsq9@gmail.com>
2021-03-04 17:33:50 +0200

    -Add 2 property in model of Game -Add 2 model

--- Game_Step/Models/Game.cs
+++ Game_Step/Models/Game.cs
@@ -5,6 +5,8 @@ namespace Game_Step.Models
     public class Game
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
         public string Article { get; set; }
         public string Language { get; set; }
         public DateTime ReleaseDate { get; set; }
---
+++ Game_Step/Models/MinimumSystemRequirements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game_Step.Models
+{
+    public class MinimumSystemRequirements
+    {
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+        public string OC { get; set; }
+        public string CPU { get; set; }
+        public string RAM { get; set; }
+        public string VideoCard { get; set; }
+        public string DirectX { get; set; }
+        public string HDD { get; set; }
+    }
+}
