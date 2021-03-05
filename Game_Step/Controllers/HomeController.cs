03f258d728c82d5766170e6e62352efc08fb7f78
Betsq9 <betsq9@gmail.com>
2021-03-05 13:36:05 +0200

    -Drop Privacy -Delete migration

--- Game_Step/Controllers/HomeController.cs
+++ Game_Step/Controllers/HomeController.cs
@@ -18,12 +18,7 @@ namespace Game_Step.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+    
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
