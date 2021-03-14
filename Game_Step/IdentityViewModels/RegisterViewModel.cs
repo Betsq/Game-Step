c0e59a0cbff507e44d42251e29751208794270f2
Betsq9 <betsq9@gmail.com>
2021-03-14 23:58:55 +0200

    -Update Register model. Added properties

--- Game_Step/IdentityViewModels/RegisterViewModel.cs
+++ Game_Step/IdentityViewModels/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,27 @@ namespace Game_Step.IdentityViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Password mismatch")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
     }
 }
