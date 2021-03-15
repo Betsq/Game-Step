d5f7659ead114d677c2af827370586b6121f5ee3
Betsq9 <betsq9@gmail.com>
2021-03-15 18:07:41 +0200

    -Update UserController. Added "Edit" method

--- Game_Step/Controllers/IdentityControllers/UserController.cs
+++ Game_Step/Controllers/IdentityControllers/UserController.cs
@@ -58,5 +58,57 @@ namespace Game_Step.Controllers.IdentityControllers
             }
             return View(model);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                User user = await userManager.FindByIdAsync(id);
+
+                if (user != null)
+                {
+                    EditUserViewModel model = new EditUserViewModel
+                    {
+                        Id = user.Id,
+                        Email = user.Email,
+                        FirstName = user.FirstName,
+                        LastName = user.LastName,
+                    };
+                    return View(model);
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+
+                    var result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return NotFound();
+        }
     }
 }
