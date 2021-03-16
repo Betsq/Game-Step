6edf09c1078f7da815a0016f4b7f2c8517f0fa58
Betsq9 <betsq9@gmail.com>
2021-03-16 19:33:10 +0200

    -Added EmailService class

---
+++ Game_Step/Util/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System.Threading.Tasks;
+
+namespace Game_Step.Util
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Game Step", "bubblenewsq0@gmail.com"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.gmail.com", 587, false);
+                await client.AuthenticateAsync("bubblenewsq0@gmail.com", "bubblesitenews");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
