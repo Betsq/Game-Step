87713fe80ab617b9069fee6e7e232186f5cfeb4e
Betsq9 <betsq9@gmail.com>
2021-03-16 18:15:47 +0200

    -Added class RoleInitializre

---
+++ Game_Step/Util/RoleInitializer.cs
@@ -0,0 +1,44 @@
+﻿using Game_Step.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game_Step.Util
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "admin@gmail.com";
+            string password = "123456";
+
+            if (roleManager.FindByIdAsync("Admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+            if (roleManager.FindByIdAsync("Employee") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Employee"));
+            }
+
+            if (userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User
+                {
+                    Email = adminEmail,
+                    UserName = adminEmail,
+                    EmailConfirmed = true
+                };
+
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+        }
+
+    }
+}
