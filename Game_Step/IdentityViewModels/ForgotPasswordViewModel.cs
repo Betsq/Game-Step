68b15253b83b2a335e8ea72e480f032fa14f72ff
Betsq9 <betsq9@gmail.com>
2021-03-16 22:49:46 +0200

    -Added forgot password view model -Added reset password view model

---
+++ Game_Step/IdentityViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Game_Step.IdentityViewModels
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
