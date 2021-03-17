1cf1da7854fb1460d978d41071ea90d721ae661b
Betsq9 <betsq9@gmail.com>
2021-03-17 22:17:29 +0200

    -Update "AccountChangePasswordViewModel", added confirm new password

--- Game_Step/IdentityViewModels/AccountChangePasswordViewModel.cs
+++ Game_Step/IdentityViewModels/AccountChangePasswordViewModel.cs
@@ -1,8 +1,22 @@
-﻿namespace Game_Step.IdentityViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Game_Step.IdentityViewModels
 {
     public class AccountChangePasswordViewModel
     {
+        [Required]
+        [DataType(DataType.Password)]
         public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "Password mismatch")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string ConfirmNewPassword { get; set; }
+
+        [Required]
         public string OldPassword { get; set; }
+
+        
     }
 }
