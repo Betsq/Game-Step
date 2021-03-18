ec69d185bc23c59b29aff55259bf823873738223
Betsq9 <betsq9@gmail.com>
2021-03-18 18:51:44 +0200

    -Added Cart view component. -Added foleder Components. -Addded "Default" View of CartViewComponent -Added View Component in _Layout

---
+++ Game_Step/ViewComponent/CartViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Game_Step.ViewComponent
+{
+    [ViewComponent]
+    public class CartViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
