8d1faad10d9a334dbd1af0fdbef0cfa39627eaa4
Betsq9 <betsq9@gmail.com>
2021-03-27 23:49:13 +0200

    -Update Home Page.

--- Game_Step/wwwroot/js/HomePage/HomePage.js
+++ Game_Step/wwwroot/js/HomePage/HomePage.js
@@ -4,18 +4,27 @@ $(document).ready(function () {
     $('.catalog-leaders-sells').fadeIn(1);
 
     $('.item-tabs').click(function () {
-       //Change the style on the button in order to see in which directory we are
+        //Change the style on the button in order to see in which directory we are
         $('li.item-tabs').attr('class', 'item-tabs');
         $(this).attr('class', 'item-tabs item-tabs-active');
 
         //Get the button attribute to know which directory to change to
         let getAtt = this.getAttribute('catalog');
         //Setting the animation time
-        let setTimeAnim = 500;
+        let setTimeAnim = 250;
+
+        //The function is intended to disable the button for changing directories until the animation ends
+        function disableChangeCatalog() {
+            $('li.item-tabs').css('pointer-events', 'none');
+
+            setTimeout(function () {
+                $('li.item-tabs').css('pointer-events', 'auto');
+            }, setTimeAnim * 2);
+        }
 
         //Check which directory we switch to
         if (getAtt === 'catalog1') {
-
+            disableChangeCatalog();
             //Hide the previous catalog with smooth animation
             $('.catalog-leaders-sells').fadeOut(setTimeAnim);
             $('.catalog-expected').fadeOut(setTimeAnim);
@@ -26,9 +35,9 @@ $(document).ready(function () {
             setTimeout(function () {
                 $('.catalog-novetly').fadeIn(setTimeAnim);
             }, setTimeAnim);
-
         }
         else if (getAtt === 'catalog2') {
+            disableChangeCatalog();
             $('.catalog-novetly').fadeOut(setTimeAnim);
             $('.catalog-expected').fadeOut(setTimeAnim);
 
@@ -37,13 +46,13 @@ $(document).ready(function () {
             }, setTimeAnim);
         }
         else {
+            disableChangeCatalog();
             $('.catalog-leaders-sells').fadeOut(setTimeAnim);
             $('.catalog-novetly').fadeOut(setTimeAnim);
 
             setTimeout(function () {
                 $('.catalog-expected').fadeIn(setTimeAnim);
             }, setTimeAnim);
-
         }
     })
 })
