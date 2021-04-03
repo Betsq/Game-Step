5e79344767c51340110a101632509ab52955e19c
Betsq9 <betsq9@gmail.com>
2021-04-03 12:55:40 +0300

    -Added "GameScreenshotController"

---
+++ Game_Step/Controllers/AllGameController/GameScreenshotController.cs
@@ -0,0 +1,22 @@
+﻿using Game_Step.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game_Step.Controllers.AllGameController
+{
+    public class GameScreenshotController : Controller
+    {
+        private readonly ApplicationContext db;
+        private readonly IWebHostEnvironment appEnvironment;
+
+        public GameScreenshotController(ApplicationContext context, IWebHostEnvironment appEnvironment)
+        {
+            db = context;
+            this.appEnvironment = appEnvironment;
+        }
+    }
+}
