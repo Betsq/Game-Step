734e801206b4e4abb87790e02d0365fd989d2356
Betsq9 <betsq9@gmail.com>
2021-04-04 12:23:28 +0300

    -Moved js code from "Update.cshtml" to "GameScreenshot.js"

---
+++ Game_Step/wwwroot/js/Game/GameScreenshot.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $(".del-button").click(function () {
+        let getId = $(this).attr('data-id');
+        $.ajax({
+            url: window.location.origin + "/GameScreenshot/Delete",
+            type: "Get",
+            data: { 'id': getId },
+            success: function () {
+                location.reload();
+            },
+        });
+
+    });
+});
+
+
+$(".button-add").click(function () {
+    $(this).next().trigger("click");
+});  
