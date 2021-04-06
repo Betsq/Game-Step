0cc98aeaf64bfa24cb5c8110b1267e6fcdd769fc
Betsq9 <betsq9@gmail.com>
2021-04-06 22:02:24 +0300

    -Added 2 comment models

---
+++ Game_Step/Models/Comments/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game_Step.Models.Comments
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime TimeCreated { get; set; }
+    }
+}
