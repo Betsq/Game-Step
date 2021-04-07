4509bb1f00d6d835e6c73b195b6c47b06aca3b18
Betsq9 <betsq9@gmail.com>
2021-04-07 16:21:29 +0300

    -Added "Comments" view controller -Added "AddMainComment"method in the "Comment" controller

---
+++ Game_Step/Controllers/AllGameController/CommentController.cs
@@ -0,0 +1,37 @@
+﻿using Game_Step.Models;
+using Game_Step.Models.Comments;
+using Game_Step.ViewModels.GamesViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Game_Step.Controllers.AllGameController
+{
+    public class CommentController : Controller
+    {
+        private readonly ApplicationContext db;
+
+        public CommentController(ApplicationContext db)
+        {
+            this.db = db;
+        }
+
+        public async System.Threading.Tasks.Task<IActionResult> AddMainComment(CommentsViewModel model)
+        {
+            var game = await db.Games.FirstOrDefaultAsync(item => item.Id == model.GameId);
+            if (game != null)
+            {
+                MainComment mainComment = new MainComment
+                {
+                    Message = model.Message,
+                    TimeCreated = DateTime.Now,
+                    Game = game,
+                };
+            }
+
+            return RedirectToAction();
+        }
+    }
+}
+
+
---
+++ Game_Step/ViewModels/GamesViewModel/CommentsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Game_Step.Models.Comments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game_Step.ViewModels.GamesViewModel
+{
+    public class CommentsViewModel : Comment
+    {
+        public int GameId { get; set; }
+    }
+}
