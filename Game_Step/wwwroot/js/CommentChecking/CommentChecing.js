ef448592f3f4c432e663ea78133b379bd8e79a2e
Betsq9 <betsq9@gmail.com>
2021-04-10 23:39:36 +0300

    Add script
    Add folder with script js for comment controller
    
    Add js script to remove comment from moderator panel

---
+++ Game_Step/wwwroot/js/CommentChecking/CommentChecing.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $(".del-comment").click(function () {
+        const id = $(this).attr("data-id");
+        const thisBtn = $(this);
+        $.ajax({
+            url: window.location.origin + "/Comment/RemoveMainComment",
+            type: "POST",
+            data: { "id": id },
+            success: function (result) {
+                if (result === true) {
+                    thisBtn.parent().remove();
+                }
+                else {
+                    alert("Ошибка, комментарий не удален!");
+                }
+            },
+        });
+    });
+});
