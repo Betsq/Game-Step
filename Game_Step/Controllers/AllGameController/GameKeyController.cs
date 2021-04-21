39106a11631275b1c469746dd1a12de3f8b752b2
Betsq <betsq9@gmail.com>
2021-04-21 23:22:59 +0300

    Change 'AddKey' method

--- Game_Step/Controllers/AllGameController/GameKeyController.cs
+++ Game_Step/Controllers/AllGameController/GameKeyController.cs
@@ -10,12 +10,12 @@ namespace Game_Step.Controllers
 {
     public class GameKeyController : Controller
     {
-        private readonly ApplicationContext db;
+        private readonly ApplicationContext _db;
         private readonly IWebHostEnvironment _appEnvironment;
 
         public GameKeyController(ApplicationContext context, IWebHostEnvironment appEnvironment)
         {
-            db = context;
+            _db = context;
             _appEnvironment = appEnvironment;
         }
 
@@ -33,124 +33,54 @@ namespace Game_Step.Controllers
         [HttpPost]
         public async Task<IActionResult> AddKey(GameKey gameKey, IFormFile uploadFile)
         {
-            if (uploadFile != null)
+            if (uploadFile == null)
+                return RedirectToAction("Index");
+
+            //Uploading the file to the server
+            string path = uploadFile.FileName;
+            using (var fs = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
             {
-                //Uploading the file to the server
-                string path = uploadFile.FileName;
-                using (var fs = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                await uploadFile.CopyToAsync(fs);
+
+                fs.Close();
+            }
+
+            using (var fs = new StreamReader(_appEnvironment.WebRootPath + path))
+            {
+                var game = _db.Games.FirstOrDefault(item => item.Id == gameKey.GameId);
+
+                if (game == null)
+                    return NotFound();
+
+                while (true)
                 {
-                    await uploadFile.CopyToAsync(fs);
+                    string s = await fs.ReadLineAsync();
 
-                    fs.Close();
-                }
+                    if (s == null)
+                        break;
 
-                //Reading the file
-                using (StreamReader fs = new StreamReader(_appEnvironment.WebRootPath + path))
-                {
-                    //Looking for a game for which the keys were intended
-                    var game = db.Games.FirstOrDefault(item => item.Id == (int)gameKey.GameId);
-
-                    while (true)
+                    //Creating the model
+                    var gk = new GameKey()
                     {
-                        //Read the file line by line
-                        string s = await fs.ReadLineAsync();
+                        Game = game,
+                        KeyToGame = s,
+                    };
 
-                        //If the lines run out, break the while
-                        if (s == null)
-                            break;
+                    await _db.GameKeys.AddAsync(gk);
+                    await _db.SaveChangesAsync();
 
-                        //Creating the model
-                        var gk = new GameKey()
-                        {
-                            Game = game,
-                            KeyToGame = s,
-                        };
-
-                        await db.GameKeys.AddAsync(gk);
-                        await db.SaveChangesAsync();
-
-                    }
-                    fs.Close();
                 }
+                fs.Close();
+            }
 
-                //Check for the presence of a file, if there is one, delete it
-                FileInfo fi = new FileInfo(_appEnvironment.WebRootPath + path);
-                if (fi.Exists)
-                {
-                    fi.Delete();
-                }
+            //Check for the presence of a file, if there is one, delete it
+            FileInfo fi = new FileInfo(_appEnvironment.WebRootPath + path);
+            if (fi.Exists)
+            {
+                fi.Delete();
             }
 
             return RedirectToAction("Index");
         }
-
-        //[HttpGet]
-        //public async Task<IActionResult> Read(int? id)
-        //{
-        //    if (id != null)
-        //    {
-        //        var developer = await db.Developers.FirstOrDefaultAsync(item => item.Id == id);
-        //        if (developer != null)
-        //        {
-        //            return View(developer);
-        //        }
-        //    }
-        //    return NotFound();
-        //}
-
-        //[HttpGet]
-        //public async Task<IActionResult> Update(int? id)
-        //{
-        //    if (id != null)
-        //    {
-        //        var developer = await db.Developers.FirstOrDefaultAsync(item => item.Id == id);
-        //        if (developer != null)
-        //        {
-        //            return View(developer);
-        //        }
-        //    }
-        //    return NotFound();
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> Update(Developer developer)
-        //{
-        //    db.Developers.Update(developer);
-        //    await db.SaveChangesAsync();
-
-        //    return RedirectToAction("Index");
-        //}
-
-        //[HttpGet]
-        //[ActionName("Delete")]
-        //public async Task<IActionResult> ConfirmDelete(int? id)
-        //{
-        //    if (id != null)
-        //    {
-        //        var developer = await db.Developers.FirstOrDefaultAsync(item => item.Id == id);
-        //        if (developer != null)
-        //        {
-        //            return View(developer);
-        //        }
-        //    }
-        //    return NotFound();
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id != null)
-        //    {
-        //        var developer = await db.Developers.FirstOrDefaultAsync(item => item.Id == id);
-        //        if (developer != null)
-        //        {
-        //            db.Developers.Remove(developer);
-        //            await db.SaveChangesAsync();
-
-        //            return RedirectToAction("Index");
-        //        }
-        //    }
-        //    return NotFound();
-        //}
     }
 }
