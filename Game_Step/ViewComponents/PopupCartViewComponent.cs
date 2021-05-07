b595419c4514946c74480d4245f30ce077082891
Betsq <betsq9@gmail.com>
2021-05-08 00:58:15 +0300

    View component format -Remove "cart" model because it is not needed -Change view

--- Game_Step/ViewComponents/PopupCartViewComponent.cs
+++ Game_Step/ViewComponents/PopupCartViewComponent.cs
@@ -2,46 +2,37 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Game_Step.ViewComponent
 {
-
     public class PopupCartViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
     {
-        private readonly ApplicationContext db;
+        private readonly ApplicationContext _db;
         public PopupCartViewComponent(ApplicationContext context)
         {
-            db = context;
+            _db = context;
         }
 
         public IViewComponentResult Invoke()
         {
             var listId = HttpContext.Session.Get<List<int>>("CartId");
-            List<Cart> carts = new List<Cart>();
-            if (listId != null)
+            var gameList = new List<Game>();
+
+            if (listId == null)
+                return View(gameList);
+
+            foreach (var id in listId)
             {
-                foreach (var id in listId)
-                {
-                    var game = db.Games.Find(id);
-                    var priceGame = db.GamePrices.FirstOrDefault(item => item.GameId == id);
-                    if (game != null)
-                    {
-                        Cart cart = new Cart
-                        {
-                            Id = game.Id,
-                            Name = game.Name,
-                            Price = priceGame.Price,
-                            Quantity = game.QuantityOfGoods,
-                            IsDiscount = priceGame.IsDiscount,
-                            Discount = priceGame.Discount,
-                            DiscountPrice = priceGame.DiscountPrice,
-                        };
-                        carts.Add(cart);
-                    }
-                }
+                var game = _db.Games
+                    .Include(item => item.GamePrice)
+                    .FirstOrDefault(item => item.Id == id);
+
+                if (game != null)
+                    gameList.Add(game);
             }
 
-            return View(carts);
+            return View(gameList);
         }
     }
 }
