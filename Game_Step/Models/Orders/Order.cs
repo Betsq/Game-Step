ff87d0725043ac2064c025afcdbfaa75a6b43685
Betsq <betsq9@gmail.com>
2021-05-09 11:50:11 +0300

    Create folder and move files

--- Game_Step/Models/Orders/Order.cs
+++ Game_Step/Models/Orders/Order.cs
@@ -1,10 +1,12 @@
-﻿namespace Game_Step.Models
+﻿using System.Collections.Generic;
+
+namespace Game_Step.Models
 {
     public class Order
     {
         public int Id { get; set; }
-
         public int ProductId { get; set; }
+        public string ProductName { get; set; }
         public int ProductPrice { get; set; }
         public int AmountProduct { get; set; }
 
